77a33ccfd987ff47088fcfdd2361df033442877c
mateobode <mateo_bode@yahoo.com>
2023-01-30 12:39:20 +0100

    Added Student Course serializer

--- backend/serializers/student.py
+++ backend/serializers/student.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+
+from backend.models.course import Course
 from backend.models.student import Student
 
 
@@ -6,3 +8,11 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = "__all__"
+
+
+class StudentCourseSerializer(serializers.ModelSerializer):
+    student = StudentSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Course
+        fields = ['student', 'name']
--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,11 @@
 from backend.views.student import StudentViewSet
 from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+
 
 router = DefaultRouter()
 router.register(r'students', StudentViewSet)
-urlpatterns = router.urls
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
