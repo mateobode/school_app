f0358b9e72ebd24862f5bbc2815027919e5ad3ea
mateobode <mateo_bode@yahoo.com>
2023-02-09 14:43:01 +0100

    added api to export grades

--- backend/urls.py
+++ backend/urls.py
@@ -2,8 +2,10 @@ from backend.views.assignment import AssignmentViewSet
 from backend.views.course import CourseViewSet
 from backend.views.student import StudentViewSet
 from backend.views.teacher import TeacherViewSet
-from rest_framework.routers import DefaultRouter
+from backend.views.reports import export_student_grades
 
+from rest_framework.routers import DefaultRouter
+from django.urls import path
 
 router = DefaultRouter()
 router.register(r'students', StudentViewSet)
@@ -11,4 +13,8 @@ router.register(r'teachers', TeacherViewSet)
 router.register(r'courses', CourseViewSet)
 router.register(r'assignments', AssignmentViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('grade-report/', export_student_grades),
+]
+
+urlpatterns += router.urls
---
+++ backend/views/reports.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+import pandas as pd
+from backend.models.assignment import Assignment
+
+
+@api_view(['get'])
+def export_student_grades(request):
+
+    if pk := request.GET.get('pk'):
+        assignments = Assignment.objects.filter(student__pk=pk)
+    else:
+        assignments = Assignment.objects.all()
+
+    data = []
+
+    for assignment in assignments:
+        data.append({
+            "student": assignment.student.first_name,
+            "description": assignment.description,
+            "grade": assignment.grade
+        })
+
+    response = HttpResponse( pd.DataFrame(data).to_csv(), content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=grades_report.csv'
+    return response
